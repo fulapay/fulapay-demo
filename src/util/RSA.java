8ff2e24eed82e440e0e89e87d5bd8b7af0f2ed5a
无名 <952532573@qq.com>
2016-12-08 11:48:26 +0800

    update

--- src/util/RSA.java
+++ src/util/RSA.java
@@ -8,6 +8,7 @@ import java.io.InputStream;
 import java.security.KeyFactory;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.security.Signature;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
 
@@ -28,15 +29,10 @@ public class RSA {
             PKCS8EncodedKeySpec priPKCS8 = new PKCS8EncodedKeySpec(Base64.decode(privateKey));
             KeyFactory keyf = KeyFactory.getInstance("RSA");
             PrivateKey priKey = keyf.generatePrivate(priPKCS8);
-
-            java.security.Signature signature = java.security.Signature
-                    .getInstance(SIGN_ALGORITHMS);
-
+            Signature signature = Signature.getInstance(SIGN_ALGORITHMS);
             signature.initSign(priKey);
             signature.update(content.getBytes(input_charset));
-
             byte[] signed = signature.sign();
-
             return Base64.encode(signed);
         } catch (Exception e) {
             e.printStackTrace();
@@ -50,7 +46,7 @@ public class RSA {
      *
      * @param content        待签名数据
      * @param sign           签名值
-     * @param ali_public_key 支付宝公钥
+     * @param ali_public_key 付啦公钥
      * @param input_charset  编码格式
      * @return 布尔值
      */
@@ -59,17 +55,10 @@ public class RSA {
             KeyFactory keyFactory = KeyFactory.getInstance("RSA");
             byte[] encodedKey = Base64.decode(ali_public_key);
             PublicKey pubKey = keyFactory.generatePublic(new X509EncodedKeySpec(encodedKey));
-
-
-            java.security.Signature signature = java.security.Signature
-                    .getInstance(SIGN_ALGORITHMS);
-
+            Signature signature = Signature.getInstance(SIGN_ALGORITHMS);
             signature.initVerify(pubKey);
             signature.update(content.getBytes(input_charset));
-
-            boolean bverify = signature.verify(Base64.decode(sign));
-            return bverify;
-
+            return signature.verify(Base64.decode(sign));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -87,16 +76,13 @@ public class RSA {
      */
     public static String decrypt(String content, String private_key, String input_charset) throws Exception {
         PrivateKey prikey = getPrivateKey(private_key);
-
         Cipher cipher = Cipher.getInstance("RSA");
         cipher.init(Cipher.DECRYPT_MODE, prikey);
-
         InputStream ins = new ByteArrayInputStream(Base64.decode(content));
         ByteArrayOutputStream writer = new ByteArrayOutputStream();
         //rsa解密的字节大小最多是128，将需要解密的内容，按128位拆开解密
         byte[] buf = new byte[128];
         int bufl;
-
         while ((bufl = ins.read(buf)) != -1) {
             byte[] block = null;
 
@@ -111,7 +97,6 @@ public class RSA {
 
             writer.write(cipher.doFinal(block));
         }
-
         return new String(writer.toByteArray(), input_charset);
     }
 
@@ -123,17 +108,11 @@ public class RSA {
      * @throws Exception
      */
     public static PrivateKey getPrivateKey(String key) throws Exception {
-
         byte[] keyBytes;
-
         keyBytes = Base64.decode(key);
-
         PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(keyBytes);
-
         KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-
         PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
-
         return privateKey;
     }
 }
