e3a07314c33950cbb99d7e74383985e2424d3fda
无名 <952532573@qq.com>
2016-12-12 19:27:35 +0800

    update

--- src/service/sms/Sms.java
+++ src/service/sms/Sms.java
@@ -16,8 +16,9 @@ public class Sms {
     public static void main(String[] args) {
         SortedMap<String, String> param = new TreeMap();
         param.put("service", Config.SMS_SEND);
-        param.put("tpl_id", "15"); // 测试环境无需修改，真实环境根据付啦开放后台申请的模板ID真实填写
-        param.put("content", "我是短信测试"); // 短信内容
+        param.put("tpl_id", "14"); // 测试环境无需修改，真实环境根据付啦开放后台申请的模板ID真实填写
+        // tpl 您本次操作的验证码为#number#，请在#n#分钟内有效。
+        param.put("content", "您本次操作的验证码为123456"); // 短信内容
         param.put("mobile", "13123936686"); // 要发送到的手机号
         // 商户构建请求参数
         System.out.println(">>>>post sign map: " + param);
