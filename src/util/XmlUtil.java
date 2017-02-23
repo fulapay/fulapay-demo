04f449df752cef8b9d72581d45c5884881466c57
[shier] <[shier@ixiye.com]>
2017-02-23 22:23:48 +0800

    编码集修改

--- src/util/XmlUtil.java
+++ src/util/XmlUtil.java
@@ -12,65 +12,67 @@ import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class XmlUtil {
-	/**
-	 * 解析xml,返回第一级元素键值对。如果第一级元素有子节点，则此节点的值是子节点的xml数据。
-	 * @param xmlStr
-	 * @return
-	 */
-	public static SortedMap doXMLParse(String xmlStr) throws Exception {
+    /**
+     * 解析xml,返回第一级元素键值对。如果第一级元素有子节点，则此节点的值是子节点的xml数据。
+     *
+     * @param xmlStr
+     * @return
+     */
+    public static SortedMap doXMLParse(String xmlStr) throws Exception {
         SortedMap map = new TreeMap();
         if (null == xmlStr || "".equals(xmlStr)) {
-			return map;
-		}
-		InputStream in = String2Inputstream(xmlStr);
-		SAXBuilder builder = new SAXBuilder();
-		Document doc = builder.build(in);
-		Element root = doc.getRootElement();
-		List list = root.getChildren();
-		Iterator it = list.iterator();
-		while (it.hasNext()) {
-			Element e = (Element) it.next();
-			String k = e.getName();
-			String v = "";
-			List children = e.getChildren();
-			if (children.isEmpty()) {
-				v = e.getTextNormalize();
-			} else {
-				v = getChildrenText(children);
-			}
-			map.put(k, v);
-		}
-		// 关闭流
-		in.close();
-		return map;
-	}
+            return map;
+        }
+        InputStream in = String2Inputstream(xmlStr);
+        SAXBuilder builder = new SAXBuilder();
+        Document doc = builder.build(in);
+        Element root = doc.getRootElement();
+        List list = root.getChildren();
+        Iterator it = list.iterator();
+        while (it.hasNext()) {
+            Element e = (Element) it.next();
+            String k = e.getName();
+            String v = "";
+            List children = e.getChildren();
+            if (children.isEmpty()) {
+                v = e.getTextNormalize();
+            } else {
+                v = getChildrenText(children);
+            }
+            map.put(k, v);
+        }
+        // 关闭流
+        in.close();
+        return map;
+    }
 
-	/**
-	 * 获取子结点的xml
-	 * @param children
-	 * @return String
-	 */
-	public static String getChildrenText(List children) {
-		StringBuffer sb = new StringBuffer();
-		if (!children.isEmpty()) {
-			Iterator it = children.iterator();
-			while (it.hasNext()) {
-				Element e = (Element) it.next();
-				String name = e.getName();
-				String value = e.getTextNormalize();
-				List list = e.getChildren();
-				sb.append("<" + name + ">");
-				if (!list.isEmpty()) {
-					sb.append(getChildrenText(list));
-				}
-				sb.append(value);
-				sb.append("</" + name + ">");
-			}
-		}
-		return sb.toString();
-	}
+    /**
+     * 获取子结点的xml
+     *
+     * @param children
+     * @return String
+     */
+    public static String getChildrenText(List children) {
+        StringBuffer sb = new StringBuffer();
+        if (!children.isEmpty()) {
+            Iterator it = children.iterator();
+            while (it.hasNext()) {
+                Element e = (Element) it.next();
+                String name = e.getName();
+                String value = e.getTextNormalize();
+                List list = e.getChildren();
+                sb.append("<" + name + ">");
+                if (!list.isEmpty()) {
+                    sb.append(getChildrenText(list));
+                }
+                sb.append(value);
+                sb.append("</" + name + ">");
+            }
+        }
+        return sb.toString();
+    }
 
-	public static InputStream String2Inputstream(String str) {
-		return new ByteArrayInputStream(str.getBytes());
-	}
+    public static InputStream String2Inputstream(String str) throws Exception {
+        return new ByteArrayInputStream(str.getBytes("UTF-8"));
+    }
 }
