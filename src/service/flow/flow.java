f947f508a394d301afce9cf6e1c0d07f08414020
[shier] <[shier@ixiye.com]>
2017-03-06 15:16:54 +0800

    update

---
+++ src/service/flow/flow.java
@@ -0,0 +1,39 @@
+package service.flow;
+
+import config.Config;
+import util.HttpsUtil;
+import util.PayUtil;
+import util.XmlUtil;
+
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+/**
+ * Created by 87119 on 2017/3/4.
+ */
+public class flow {
+    public static void main(String[] args) {
+        SortedMap<String, String> param = new TreeMap();
+        param.put("service", "flow.buyflow");
+        param.put("mobile", "18658161306");
+        param.put("flow_id", "500000020");
+        System.out.println(">>>>post sign map: " + param);
+        String xmlStr = PayUtil.buildRequestXml(param);
+        System.out.println(">>>>post xmlStr: " + xmlStr);
+        String resText = HttpsUtil.post("http://localhost:8080/flow/buyFlow", xmlStr, Config.CHARSET);
+        System.out.println("<<<<resText: " + resText);
+        // 验签后取得支付数据
+        try {
+            SortedMap<String, String> result = XmlUtil.doXMLParse(resText);
+            System.out.println("<<<<res map: " + result);
+            if (PayUtil.verifyFulaParam(result)) {
+                System.out.println("<<<<verify success-----------------");
+                System.out.println("<<<<流量充值结果:" + result);
+            } else {
+                System.out.println("<<<<verify fail-----------------");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
