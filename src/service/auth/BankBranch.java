8b12c5d57273a0124d3c759165d88abbe7abe024
[shier] <[shier@ixiye.com]>
2017-05-11 14:55:28 +0800

    update

--- src/service/auth/BankBranch.java
+++ src/service/auth/BankBranch.java
@@ -1,6 +1,7 @@
 package service.auth;
 
 import config.Config;
+import net.sf.json.JSONArray;
 import org.junit.Test;
 import util.HttpsUtil;
 import util.PayUtil;
@@ -8,7 +9,6 @@ import util.XmlUtil;
 
 import java.util.SortedMap;
 import java.util.TreeMap;
-import java.util.UUID;
 
 /**
  * Created by 87119 on 2017/5/11.
@@ -19,9 +19,9 @@ public class BankBranch {
     public void banckBranch() {
         SortedMap<String, String> param = new TreeMap();
         param.put("service", Config.AUTH_BANK_BRANCH);
-        param.put("province_name", "1");
-        param.put("city_name", "马云");
-        param.put("bank_name", "621481273457448");
+        param.put("province_name", "浙江省");
+        param.put("city_name", "杭州市");
+        param.put("bank_name", "中国银行");
         System.out.println(">>>>post sign map: " + param);
         String xmlStr = PayUtil.buildRequestXml(param);
         System.out.println(">>>>post xmlStr: " + xmlStr);
@@ -30,7 +30,12 @@ public class BankBranch {
         // 验签后取得支付数据
         try {
             SortedMap<String, String> result = XmlUtil.doXMLParse(resText);
-            System.out.println("<<<<res map: " + result);
+            JSONArray bankBranch = JSONArray.fromObject(result.get("data"));
+            System.out.println("<<<<res data: " + bankBranch);
+            Object[] list = bankBranch.toArray();
+            for (Object s : list) {
+                System.out.println(s);
+            }
             if (PayUtil.verifyFulaParam(result)) {
                 System.out.println("<<<<verify success-----------银行支行:" + result);
             } else {
